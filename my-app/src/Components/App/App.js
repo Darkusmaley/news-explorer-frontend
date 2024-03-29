1876bf9b8f34bb8b6344b1d48c712b03213f6915
Darkusmaley <daleymarcus091@gmail.com>
2024-03-29 18:37:54 -0400

    added folders for components, images, utils and fonts

---
+++ my-app/src/Components/App/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Route>"/"- main</Route> */}
+      {/* <Route>"/saved-news"- displays Saved articles page</Route> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Header/Header.js
@@ -0,0 +1,73 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+const Header = ({}) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <header className="header app__section">
+      <div className="header__logo">
+        <div>
+          <Link to="/">
+            <img className="header__logo-image" src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="header__date">
+          {currentDate}, {location}
+        </div>
+      </div>
+
+      <div className="header__avatar">
+        <ToggleSwitch />
+        {isLoggedIn ? (
+          <>
+            <div>
+              <button
+                className="header__clothes-add-button"
+                type=" text"
+                onClick={onCreateModal}
+                id="add-clothes-button"
+              >
+                + Add new
+              </button>
+            </div>
+            <div>
+              <Link to="/profile" className="profile__link">
+                {currentUser?.name}
+              </Link>
+            </div>
+            <div>
+              <img
+                className="header__logo-avatar-image"
+                src={currentUser?.avatar}
+                placeholder={currentUser?.name}
+                alt="Avatar logo"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <button
+              type="button"
+              onClick={onRegister}
+              className="header__button"
+            >
+              Sign up
+            </button>
+            <button type="button" onClick={onLogin} className="header__button">
+              Login{" "}
+            </button>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
+
+const currentDate = new Date().toLocaleString("default", {
+  month: "long",
+  day: "numeric",
+});
+
+export default Header;
