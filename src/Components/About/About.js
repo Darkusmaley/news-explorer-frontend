17af2b5b2612a696c1cea36c580ac5a927e8c0ad
Darkusmaley <daleymarcus091@gmail.com>
2024-04-04 15:51:51 -0400

    feat/about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,14 +3,17 @@ import "./About.css";
 function About() {
   return (
     <about className="about app__section">
-      <div>
-        <h2 className="about__title">About the author</h2>
-        <h3 className="about__subtext">
-          This block describes the project author. Here you should indicate your
-          name, what you do, and which development technologies you know. You
-          can also talk about your experience with TripleTen, what you learned
-          there, and how you can help potential customers.
-        </h3>
+      <div className="about__info">
+        <img src="" alt="Picture of author" className="about__image" />
+        <div className="about__text">
+          <h2 className="about__title">About the author</h2>
+          <h3 className="about__subtext">
+            This block describes the project author. Here you should indicate
+            your name, what you do, and which development technologies you know.
+            You can also talk about your experience with TripleTen, what you
+            learned there, and how you can help potential customers.
+          </h3>
+        </div>
       </div>
     </about>
   );
