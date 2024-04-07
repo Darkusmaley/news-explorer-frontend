2545fea6755b13fbeb5bda28bd78f13f0a8b766d
Darkusmaley <daleymarcus091@gmail.com>
2024-04-06 20:18:46 -0400

    feat/preloader

--- src/App.js
+++ src/App.js
@@ -6,17 +6,48 @@ import About from "./Components/About/About";
 import Footer from "./Components/Footer/Footer";
 import React from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import { Routes } from "react-router";
 
 function App() {
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  const handleLoginModal = () => {
+    setActiveModal("login");
+  };
+
+  const handleRegisterModal = () => {
+    setActiveModal("register");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
+  const handleSelectedCard = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  const handleSubmit = (request) => {
+    setIsLoading(true);
+    request()
+      .then(handleCloseModal)
+      .catch(console.error)
+      .finally(() => setIsLoading(false));
+  };
+
   return (
     <div className="App ">
       <Routes>
-        <Route path="/" element={<Main />}></Route>
-        {/* <Route>"/saved-news"- displays Saved articles page</Route> */}
+        <Route exact path="/" element={<Main />}></Route>
+        {/* <Route path="/bookmarked">"/saved-news"- displays Saved articles page</Route> */}
       </Routes>
-      <About></About>
+      <About />
       <Footer />
     </div>
   );
