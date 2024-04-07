2bf29af82713461f59e2e696e7f65bdd20d738f2
Darkusmaley <daleymarcus091@gmail.com>
2024-04-06 21:54:05 -0400

    feat/searchbar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,6 @@
 import "./Main.css";
 import Header from "../Header/Header";
+import SearchBar from "../SearchForm/SearchForm";
 
 // import { useMemo, useContext } from "react";
 
@@ -17,12 +18,13 @@ function Main({ onSelectCard, isLoggedIn }) {
   return (
     <main className="main ">
       <div className="main__overlay">
-        <Header></Header>
+        <Header />
         <h1 className="main__title">What's going on in the world? </h1>
         <h3 className="main__subtext">
           Find the latest news on any topic and save them in your personal
           account.
         </h3>
+        <SearchBar />
       </div>
     </main>
   );
---
+++ src/Components/SearchForm/SearchForm.js
@@ -0,0 +1,18 @@
+import "./SearchForm.css"
+
+function SearchBar() {
+  return (
+    <div className="searchbar">
+      <form className="searchbar__form">
+        <input
+          type="search"
+          className="searchbar__input"
+          placeholder="Enter topic"
+        ></input>
+        <button className="searchbar__button"> Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
