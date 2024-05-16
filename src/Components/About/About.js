a358e33ae58cc715eeee83f1841a1047e553cb79
Darkusmaley <daleymarcus091@gmail.com>
2024-05-16 01:55:38 -0400

    refactored about.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,10 +8,14 @@ function About() {
         <div className="about__text">
           <h2 className="about__title">About the author</h2>
           <h3 className="about__subtext">
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
-            You can also talk about your experience with TripleTen, what you
-            learned there, and how you can help potential customers.
+            Hello! My name is Marcus Daley, a software developer specialized in
+            full-stack technologies such React and JavaScript. Being new to the
+            tech field, TripleTen has provided me with the skills and experience
+            to tackle complicated and challenging projects such as this one. I
+            was introduced to software engineering through several of my friends
+            and happened to stumble upon TripleTen's bootcamp. From there I
+            honed the skills neccessary to develop both front and backend
+            projects, along with working with databases and cloud servers.
           </h3>
         </div>
       </div>
