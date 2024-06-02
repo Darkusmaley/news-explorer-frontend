0355f5e9eba5efed5766e14975d0020f93e1d1ee
Darkusmaley <daleymarcus091@gmail.com>
2024-06-02 19:02:07 -0400

    deleted all

--- src/Utils/NewsApi.js
+++
@@ -1,7 +0,0 @@
-import { checkResponse } from "./CheckResponse";
-import { ApiKey, parsePreviousWeek, parseCurrentDate } from "./Constants";
-
-export const getSearchResults = (keyword) => {
-  return fetch(`https://newsapi.org/v2/everything?1=${keyword}&from=${parsePreviousWeek}&to=${parseCurrentDate}&pageSize=100&sortBy=popularity&apiKey=${ApiKey}
-    `).then(checkResponse);
-};
